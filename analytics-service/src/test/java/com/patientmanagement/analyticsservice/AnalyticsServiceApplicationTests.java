da40d60752214bbeefd76a436f4b71e52fced3a4
Divyaraj8284 <divraj.sri96@gmail.com>
2025-09-07 19:22:37 +0530

    aaded api gateway for docs

---
+++ analytics-service/src/test/java/com/patientmanagement/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patientmanagement.analyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
